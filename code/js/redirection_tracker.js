7ed131d77acb2837c592e0b0a59946d6e8e53d8f
Nick Schorr <naschorr@gmail.com>
2017-02-06 02:37:05 -0500

    Split RegexLookup and RedirectionTracker into their own scripts
    - Added RegexLookup's tests into it's own test spec

---
+++ code/js/redirection_tracker.js
@@ -0,0 +1,48 @@
+/**
+ * Class that handles a limited redirection history and redirection checking
+ * TODO: Add tests for this class
+ */
+class RedirectionTracker {
+  /**
+   * Creates a private dictionary to hold the tab redirection data
+   */
+  constructor() {
+    this._tabs = {};
+  }
+
+  /**
+   * Adds a redirection source to the given tab
+   * @param {int} tabId - The tab's integer id
+   * @param {string} url - The url of the last page redirected from
+   */
+  addRedirection(tabId, url) {
+    this._tabs[tabId] = url;
+  }
+
+  /**
+   * Gets the most recent redirection source done in the given tab
+   * @param {int} tabId - The tab's integer id
+   * @return {string} The url of the last page redirected from
+   */
+  getRedirection(tabId) {
+    return this._tabs[tabId];
+  }
+
+  /**
+   * Checks if the given tab can redirect to the current URL
+   * @param {int} tabId - The tab's integer id
+   * @param {string} url - The url of the active tab
+   * @return {boolean} True if redirection is possible, False if it isn't
+   */
+  canRedirect(tabId, url) {
+    return (url != this.getRedirection(tabId));
+  }
+
+  /**
+   * Removes a tab's key from the dictionary
+   * @param {int} tabId - The tab's integer id
+   */
+  remove(tabId) {
+    delete this._tabs[tabId];
+  }
+}
